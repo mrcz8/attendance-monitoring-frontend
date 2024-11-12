f40ef50f2695f12599bd52fda305a8b2de286b93
Marc <tech9@sumomedia.co>
2024-11-12 09:56:40 +0800

    add chart

---
+++ components/PieChart.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Chart :type="'doughnut'" :data="chartData" :options="chartOptions" />
+    </div>
+</template>
+
+<script>
+import { Chart } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement);
+
+export default {
+    components: {
+        Chart,
+    },
+    props: {
+        chartData: {
+            type: Object,
+            required: true,
+        },
+        chartOptions: {
+            type: Object,
+            default: () => ({}),
+        },
+    },
+};
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -89,5 +89,12 @@ export default {
           autoprefixer: {},
         },
       },
+    extend(config) {
+        config.module.rules.push({
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules\/(?!chart.js|vue-chartjs)/,
+        });
+      },
   },
 }
